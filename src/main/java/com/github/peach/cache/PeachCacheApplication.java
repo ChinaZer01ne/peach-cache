310665c869bc863487832dbef9ef943eed72397e
taofengfeng <taofengfeng>
2020-11-12 13:45:49 +0800

    Initial commit

---
+++ src/main/java/com/github/peach/cache/PeachCacheApplication.java
@@ -0,0 +1,13 @@
+package com.github.peach.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeachCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeachCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/peach/cache/PeachCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.peach.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeachCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
